103d24bfa451bae3f41e722f4fcd96d1df8552bf
Daniyar_Bulgunayev <Daniyar_Bulgunayev@epam.com>
2017-09-29 18:33:48 +0600

    trying to make validation on server side

--- src/server/index.js
+++ src/server/index.js
@@ -1,41 +1,47 @@
-const Koa = require('koa');
+const koa = require('koa');
 const cors = require('koa-cors');
-const app = new Koa();
-const middlewares = require('koa-middlewares');
-const router = require('koa-router')();
+const app = new koa();
+// const middlewares = require('koa-middlewares');
+const router = require('koa-route');
 
 app.use(cors());
 // app.use(async ctx => {
 //     ctx.body = 'Hello World';
 // });
 
-app.use(middlewares.bodyParser());
+// app.use(middlewares.bodyParser());
 // app.use(middlewares.router(app));
 
-router.get('/test', () => {
+// router.get('/test', () => {
+//     console.log(arguments);
+//     console.log(this);
+// });
+app.use(router.get('/', (ctx) => {
     console.log(arguments);
     console.log(this);
-});
-router.get('/test2', function* () {
+    ctx.body = 'Hello world!';
+}));
+
+app.use(router.get('/test1', function* () {
     console.log(arguments);
     console.log(this);
-    this.body = 'Hello world!';
-});
+    this.body = 'Hello world! test1';
+}));
+//
+// router.post('/login', (ctx) => {
+//     console.log(arguments);
+//     console.log(this);
+// });
 
-router.post('/login', (ctx) => {
-    console.log(arguments);
-    console.log(this);
-});
+app.use(router.post('/validate', (ctx, data) => {
+    // console.log(data);
+    console.log(ctx.request.body);
+    // console.log(this);
+    // console.log(this.params);
+    ctx.body = 'Ok';
+}));
 
-router.post('/validate', function* (data, next) {
-    console.log(data);
-    console.log(arguments);
-    console.log(this.request);
-    console.log(this.params);
-    this.body = 'Ok';
-});
-
-app.use(router.routes())
-    .use(router.allowedMethods());
+// app.use(router.routes())
+//     .use(router.allowedMethods());
 
 app.listen(3001);
